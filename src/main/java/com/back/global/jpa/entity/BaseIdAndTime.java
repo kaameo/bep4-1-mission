eb11523a5760a6a569c3938cd6692c8af42a4dea
kaameo <kaameo12@gmail.com>
2026-09-17 15:39:38 +0900

    0007

--- src/main/java/com/back/global/jpa/entity/BaseIdAndTime.java
+++ src/main/java/com/back/global/jpa/entity/BaseIdAndTime.java
@@ -1,6 +1,9 @@
 package com.back.global.jpa.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,7 +16,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseIdAndTime extends BaseEntity {
+public abstract class BaseIdAndTime extends BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
